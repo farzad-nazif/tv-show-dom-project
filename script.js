6bb1cd634b9e77e348d2c438c6daf857af415fbd
farzad-nazif <farzadnazif.nz@gmail.com>
2022-10-17 19:03:25 +0100

    Level 400 finished

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const url = "https://api.tvmaze.com/shows/82/episodes";
-const allEpisodes = [];
+let allEpisodes = [];
+const allShows = getAllShows();
+let episodeGrid = document.querySelector("#episodeGrid");
+let episodeSelector = document.getElementById("episodeSelector");
+let showSelect = document.getElementById("showSelect");
+let searchInput = document.getElementById("search");
+let validSearchCount = document.getElementById("validSearchCount");
+let svgIcon = document.getElementById("svgIcon");
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
@@ -7,11 +19,10 @@ fetch(url)
       allEpisodes.push(episode);
     });
   })
-  .finally(window.onload = setup);
+  .finally((window.onload = setup));
 
 // Setting up
 function setup() {
-  let episodeSelector = document.getElementById("episodeSelector");
   allEpisodes.forEach((episode) => {
     makePageForEpisodes(episode);
     // Creating options using JS, so HTMl wont get too, making big 73options elements
@@ -33,11 +44,20 @@ function setup() {
     option.appendChild(episodeNumber);
     episodeSelector.appendChild(option);
   });
+  let validShowList = [];
+  allShows.forEach((show) => {
+    validShowList.push(show.name);
+  });
+  validShowListSorted = validShowList.sort();
+  validShowListSorted.forEach((showName) => {
+    let showOption = document.createElement("option");
+    showOption.setAttribute("value", showName);
+    showOption.innerText = showName;
+    showSelect.appendChild(showOption);
+  });
 }
-
 // Creating a box of episode details
 function makePageForEpisodes(episode) {
-  let episodeGrid = document.querySelector("#episodeGrid");
   let episodeDiv = document.createElement("div");
   episodeDiv.className = "episode";
   episodeGrid.appendChild(episodeDiv);
@@ -70,12 +90,9 @@ function makePageForEpisodes(episode) {
   episodeDiv.appendChild(episodeParagraph);
   episodeParagraph.innerHTML = episode.summary;
 }
-
-let searchInput = document.getElementById("search");
 // Creating a function to filter array of objs based on the search and showing the result
 function filterAndCreateEpisodeBox(e, allEpisodes) {
   let searchWord = e.target.value;
-  let validSearchCount = document.getElementById("validSearchCount");
   let validEpisodes = allEpisodes.filter((episode) => {
     return (
       episode.name.includes(searchWord) ||
@@ -85,15 +102,13 @@ function filterAndCreateEpisodeBox(e, allEpisodes) {
     );
   });
   let validEpisodesLength = validEpisodes.length;
-  validSearchCount.innerText = `Displaying ${validEpisodesLength}/73 episodes`;
+  validSearchCount.innerText = `Displaying ${validEpisodesLength}/${allEpisodes.length} episodes`;
   // Declaring a function just to clear the page from all episodes
   function removeAllChildNodes(parent) {
     while (parent.firstChild) {
       parent.removeChild(parent.firstChild);
     }
   }
-  // Defining the parent of episodes
-  let episodeGrid = document.querySelector("#episodeGrid");
   // Deleting all episodes
   removeAllChildNodes(episodeGrid);
   // creating box of episode details
@@ -101,23 +116,15 @@ function filterAndCreateEpisodeBox(e, allEpisodes) {
     makePageForEpisodes(episode);
   });
 }
-
 // Adding Event listener for Select input
 episodeSelector.addEventListener("input", (e) => {
   let selectID = e.target.value;
-  let validSearchCount = document.getElementById("validSearchCount");
-  function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
-  }
-  let episodeGrid = document.querySelector("#episodeGrid");
   if (selectID === "false") {
     removeAllChildNodes(episodeGrid);
     allEpisodes.forEach((episode) => {
       makePageForEpisodes(episode);
     });
-    validSearchCount.innerText = `Displaying 73/73 episodes`;
+    validSearchCount.innerText = `Displaying ${allEpisodes.length}/${allEpisodes.length} episodes`;
   }
   if (selectID && selectID !== "false") {
     let selectedEpisode = allEpisodes.filter((episode) => {
@@ -129,29 +136,43 @@ episodeSelector.addEventListener("input", (e) => {
     selectedEpisode.forEach((episode) => {
       makePageForEpisodes(episode);
     });
-    validSearchCount.innerText = `Displaying 1/73 episodes`;
+    validSearchCount.innerText = `Displaying 1/${allEpisodes.length} episodes`;
   }
 });
-
-// Adding event listener for reset button
-let svgIcon = document.getElementById("svgIcon");
-svgIcon.addEventListener("click", (e) => {
-  function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
+// Adding event listener for show select
+showSelect.addEventListener("input", (e) => {
+  removeAllChildNodes(episodeGrid);
+  removeAllChildNodes(episodeSelector);
+  let showID = e.target.value;
+  let urlShow = "";
+  allShows.forEach((show) => {
+    if (show.name == showID) {
+      urlShow = `https://api.tvmaze.com/shows/${show.id}/episodes`;
+    } else if (showID === "false") {
+      urlShow = "https://api.tvmaze.com/shows/82/episodes";
     }
+  });
+  function buildNewShowList(data) {
+    setup(data);
+    validSearchCount.innerText = `Displaying ${data.length}/${data.length} episodes`;
   }
-  let episodeGrid = document.querySelector("#episodeGrid");
+  fetch(urlShow)
+    .then((response) => response.json())
+    .then((data) => {
+      allEpisodes = data;
+      buildNewShowList(data);
+    });
+});
+// Adding event listener for reset button
+svgIcon.addEventListener("click", (e) => {
   removeAllChildNodes(episodeGrid);
   allEpisodes.forEach((episode) => {
     makePageForEpisodes(episode);
   });
-  validSearchCount.innerText = `Displaying 73/73 episodes`;
+  validSearchCount.innerText = `${allEpisodes.length}/${allEpisodes.length}`;
 });
-
 // Adding event listener for Search input
 searchInput.addEventListener("input", (e) =>
   filterAndCreateEpisodeBox(e, allEpisodes)
 );
-
 window.onload = setup;
